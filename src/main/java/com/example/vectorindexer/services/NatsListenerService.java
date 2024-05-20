416f5940bbf566ab8469bbf1edb4d14d4fab3e7d
omar <omarghetti2@gmail.com>
2024-05-20 13:56:57 +0200

    Add: Nats Listener Service

--- src/main/java/com/example/vectorindexer/services/NatsListenerService.java
+++ src/main/java/com/example/vectorindexer/services/NatsListenerService.java
@@ -1,9 +1,16 @@
 package com.example.vectorindexer.services;
 
+import java.util.List;
+
+import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import io.nats.client.Connection;
+import io.nats.client.Dispatcher;
+import io.nats.client.Nats;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,9 +23,25 @@ public class NatsListenerService {
     @Autowired
     private VectorStore vectorStore;
 
+    @Value("${nats.server.url}")
+    private String natsServerUrl;
+
+    @Value("${nats.server.subject}")
+    private String natsServerSubject;
+
     @PostConstruct
     public void init() throws Exception{
         log.info("Initializing NatsListenerService...");
+        log.info("Nats server url: {}", natsServerUrl);
+        log.info("Nats server subject: {}", natsServerSubject);
+        Connection nc = Nats.connect(natsServerUrl);
+        Dispatcher natsDispatcher = nc.createDispatcher((msg) -> {
+            String message = new String(msg.getData());
+            log.info("Received message: {}", message);
+            Document document = new Document(message);
+            vectorStore.add(List.of(document));
+        });
+        natsDispatcher.subscribe(natsServerSubject);
     }
 
     
